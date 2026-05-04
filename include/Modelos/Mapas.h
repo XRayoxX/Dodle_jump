b046627d6fe8e755cea4d421e1c8b0afcc6ebb2c
AleUmg <abatenr3@miumg.edu.gt>
2026-05-03 20:44:52 -0600

    README Completo

---
+++ include/Modelos/Mapas.h
@@ -0,0 +1,16 @@
+//
+// Created by Ale on 3/05/2026.
+//
+
+#ifndef DODLE_JUMP_MAPAS_H
+#define DODLE_JUMP_MAPAS_H
+
+
+
+class Mapas {
+
+};
+
+
+
+#endif //DODLE_JUMP_MAPAS_H
---
+++ src/Modelos/Mapas.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ale on 3/05/2026.
+//
+
+#include "../../include/Modelos/Mapas.h"
