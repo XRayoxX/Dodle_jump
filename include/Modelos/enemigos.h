fe1d464817f2e9b5e9f87be97ab17c4096ae9b6b
kevinp <kevinpea22@gmail.com>
2026-05-05 22:52:26 -0600

    subo archivos de enemigos y una imagen

---
+++ include/Modelos/enemigos.h
@@ -0,0 +1,13 @@
+//
+// Created by kevin on 5/05/2026.
+//
+
+#ifndef DODLE_JUMP_ENEMIGOS_H
+#define DODLE_JUMP_ENEMIGOS_H
+
+
+class enemigos {
+};
+
+
+#endif //DODLE_JUMP_ENEMIGOS_H
---
+++ src/Modelos/enemigos.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kevin on 5/05/2026.
+//
+
+#include "../../include/Modelos/enemigos.h"
